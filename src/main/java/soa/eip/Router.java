2f92ee2f62871ee241db450c785f0e32bf9b3dbd
Andrew <aog25.2014@gmail.com>
2020-12-02 01:26:33 +0100

    max operator implemented

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,6 +1,8 @@
 package soa.eip;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +12,31 @@ public class Router extends RouteBuilder {
 
   @Override
   public void configure() {
+
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
+      .process(new MyProcessor())
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
+  }
+  private class MyProcessor implements Processor{
+    public void process(Exchange exchange) throws Exception {
+      
+      String message = exchange.getIn().getBody(String.class);
+      String splitted[] = message.split(" ");
+      String max = "";
+      for(int i = 0; i < splitted.length; i++){
+        if(splitted[i].matches("max:[0-9]+")){
+          max = splitted[i].substring(4);
+          splitted[i] = "";
+        }
+      }
+      message = String.join(" ", splitted);
+      if(!max.equals("")){
+        message += "?count=" + max;
+      }
+      exchange.getOut().setBody(message);
+    }
   }
 }
